d68422050a87f8463bac5bba05eb8b7046e5d802
Terra Vaughn <vterra28@yahoo.com>
2021-03-16 17:03:20 -0500

    Started work on Step 2 - Climate App.

---
+++ app.py
@@ -0,0 +1,81 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+###################################
+# Database Setup
+###################################
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the tables
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+###################################
+# Flask Setup
+###################################
+app = Flask(__name__)
+
+###################################
+# Flask Routes
+###################################
+
+@app.route("/")
+def index():
+    """List all available api routes."""
+    return(
+        f"Available routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+
+    session = Session(engine)
+
+    results = session.query(Measurement.date, Measurement.prcp).all()
+
+    session.close()
+
+    all_precipitation = []
+    for date, prcp in results:
+        measurement_dict = {}
+        measurement_dict["date"] = date
+        measurement_dict["prcp"] = precipitation
+        all_precipitation.append(measurement_dict)
+
+
+    return jsonify(all_precipitation)
+
+@app.route("/api/v1.0/stations")
+def stations():
+
+    session = Session(engine)
+
+    results = session.query(Station.station).all()
+
+    session.close()
+
+    all_stations = list(np.ravel(results))
+
+    return jsonify(all_stations)
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
