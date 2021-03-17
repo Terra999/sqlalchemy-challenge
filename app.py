82b3314a3cfa0066f956a2ea02e46d7849460215
Terra Vaughn <vterra28@yahoo.com>
2021-03-17 13:30:22 -0500

    Completed tobs route.

--- app.py
+++ app.py
@@ -37,7 +37,8 @@ def index():
     return(
         f"Available routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs"
     )
 
 @app.route("/api/v1.0/precipitation")
@@ -50,10 +51,11 @@ def precipitation():
     session.close()
 
     all_precipitation = []
+
     for date, prcp in results:
         measurement_dict = {}
         measurement_dict["date"] = date
-        measurement_dict["prcp"] = precipitation
+        measurement_dict["prcp"] = prcp
         all_precipitation.append(measurement_dict)
 
 
@@ -71,6 +73,29 @@ def stations():
     all_stations = list(np.ravel(results))
 
     return jsonify(all_stations)
+
+@app.route("/api/v1.0/tobs")
+def temperature():
+
+    session = Session(engine)
+
+    most_active_year = session.query(Measurement.station, Measurement.date, Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
+        filter(Measurement.date >= '2016-08-23').\
+        order_by(Measurement.date.asc()).all()
+
+    session.close()
+
+    all_temperature = []
+
+    for station, date, tobs in most_active_year:
+        tobs_dict = {}
+        tobs_dict["station"] = station
+        tobs_dict["date"] = date
+        tobs_dict["tobs"] = tobs
+        all_temperature.append(tobs_dict)
+
+    return jsonify(all_temperature)
 
 
 
